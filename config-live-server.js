3ce86b40c10b8fcc2528ff59d2f5ffb9be85bd73
WiseGuise <79070546+WiseGuise@users.noreply.github.com>
2021-06-17 16:09:48 -0500

    Update config-live-server.js

--- config-live-server.js
+++ config-live-server.js
@@ -1,23 +1,5 @@
 const fs = require("fs");
-const tls = require("tls");
 
-const options = {
-  key: fs.readFileSync("server-key.pem"),
-  cert: fs.readFileSync("server-cert.pem"),
-  requestCert: true,
-  ca: [ fs.readFileSync("client-cert.pem") ]
-};
-
-const server = tls.createServer(options, (socket) => {
-  console.log("server connected", socket.authorized ? "authorized" : "unauthorized");
-  socket.write("welcome!/n");
-  socket.setEncoding("utf8);
-  socket.pipe(socket);
-});
-
-server.listen(8000, () => {
-  console.log("server bound");
-});
 
 module.exports = {
   cert: fs.readFileSync(__dirname + "/server.cert"),
